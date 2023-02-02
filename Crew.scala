ef3ea7c1cbbc79cc4a183e8189fb389223dbdd66
DiegoJP7 <dajumbo13@utpl.edu.ec>
2023-02-02 07:36:02 -0500

    Solucion Parcial de Crew(Revisar)

--- Crew.scala
+++ Crew.scala
@@ -1,47 +1,111 @@
-package Avance1
-
-import com.cibo.evilplot.plot._
-import com.cibo.evilplot.plot.aesthetics.DefaultTheme.{DefaultElements, DefaultTheme}
-import com.github.tototoshi.csv.CSVReader
-import play.api.libs.json.{JsArray, JsValue, Json}
-
 import java.io.File
-object Crew extends App{
-  val reader = CSVReader.open(new File("/Users/diegojp/Downloads/movie_dataset 2.csv"))
+import com.github.tototoshi.csv.CSVReader
+import play.api.libs.json._
+import requests.Response
 
-  val data : List[Map[String,String]]={
+import scala.util.Try
+import scala.util.matching.Regex
+import scalikejdbc._
+
+import scalikejdbc.{AutoSession, ConnectionPool, DBSession}
+
+
+object Crew extends App {
+
+  // ------------------------------------------------------------------------------------------------
+
+  val reader = CSVReader.open(new File("/Users/diegojp/Downloads/movie_dataset 2.csv"))
+  val data: List[Map[String, String]] = {
     reader.allWithHeaders()
   }
   reader.close()
 
-  val productionCompanies = data
-  .flatMap (row => row. get ("production _companies"))
-  .map (row => Json.parse (row))
-    .flatMap (jsonData => jsonData \\ "name")
-   .map (jsValue => jsValue.as[String])
-  .groupBy(identity)
-    .map { case (keyword, lista) => (keyword, lista.size) }
-    .toList
-  .sortBy(_._2)
-  .reverse
+  def replacePattern(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "(\\s\"(.*?)\",)".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("'", "-u0027")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
 
-  val pCompaniesValues = productionCompanies.take(10).map(_._2).map(_.toDouble)
-  val pCompaniesLables = productionCompanies.take(10).map(_._1)
+  def replacePattern2(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "([a-z]\\s\"(.*?)\"\\s*[A-Z])".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("\"", "-u0022")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
 
-  implicit val theme = DefaultTheme.copy(
-    elements = DefaultElements.copy(categoricalXAxisLabelOrientation = 45)
+  def replacePattern3(original: String): String = {
+    var txtOr = original
+    val pattern: Regex = "(:\\s'\"(.*?)',)".r
+    for (m <- pattern.findAllIn(original)) {
+      val textOriginal = m
+      val replacementText = m.replace("\"", "-u0022")
+      txtOr = txtOr.replace(textOriginal, replacementText)
+    }
+    txtOr
+  }
+  
+
+  val pattern: Regex = "(\\s\"(.*?)\",)".r
+  val pattern2: Regex = "([a-z]\\s\"(.*?)\"\\s*[A-Z])".r
+  val pattern3: Regex = "(:\\s'\"(.*?)',)".r
+
+  val patternsTR = List(
+    (pattern2, "\"", "-u0022"),
+    (pattern, "'", "-u0027"),
+    (pattern3, "\"", "-u0022")
   )
 
-  BarChart(pCompaniesValues)
-    .title("Compañías productoras")
-    .xAxis(pCompaniesLables)
-    .yAxis()
-    .frame()
-    .yLabel("Productions")
-    .bottomLegend()
-    .render()
-    .write(new File ("/Users/diegojp/crew.png"))
+  def funcion(original: String, patternsTR: List[(Regex, String, String)]) = {
 
+    var txtOr = original
+    for (p <- patternsTR) {
+      for (m <- p._1.findAllIn(txtOr)) {
+        val textOriginal = m
+        val replacementText = m.replace(p._2, p._3)
+        txtOr = txtOr.replace(textOriginal, replacementText)
+      }
+    }
+    txtOr
+  }
 
-  //Wiki poner imagenes cada analisis
-}
+  val crew = data
+    .map(row => row("crew"))
+    .map(funcion(_, patternsTR))
+    .map(text => text.replace("'", "\""))
+    .map(text => text.replace("-u0027", "'"))
+    .map(text => text.replace("-u0022", "\\\""))
+    .map(text => Try(Json.parse(text))).count(_.isSuccess)
+
+  println(crew)
+
+  // ------------------------------------------------------------------------------------------------
+
+  Class.forName("com.mysql.cj.jdbc.Driver")
+  ConnectionPool.singleton("jdbc:mysql://localhost:3306/MoviesTS14", "root", "messer10")
+  implicit val session: DBSession = AutoSession
+
+  // ------------------------------------------------------------------------------------------------
+
+  def actorsNames(dataRaw: String): Option[String] = {
+    val response: Response = requests
+      .post("http://api.meaningcloud.com/topics-2.0",
+        data = Map("key" -> "4c3a20ef3efdfbf831ed7c77088f5e58",
+          "lang" -> "en",
+          "txt" -> dataRaw,
+          "tt" -> "e"),
+        headers = Map("content-type" -> "application/x-www-form-urlencoded"))
+    Thread.sleep(500)
+    if (response.statusCode == 200) {
+      Option(response.text)
+    } else
+      Option.empty
+  }
