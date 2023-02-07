7e120308a35ad17d5495b0dce71a0f0399152008
DiegoJP7 <dajumbo13@utpl.edu.ec>
2023-02-07 02:50:13 -0500

    Agregacion y Correcion de Diagramas

--- EstadisticasBasicas.scala
+++ EstadisticasBasicas.scala
@@ -19,28 +19,45 @@ object EstadisticasBasicas extends App {
   val budgetName = data.map(x => (x.get("budget"), x.get("original_title")) match {
     case (b, o) => (b.get.toDouble, o.get)
   })
-  val sumBug = budget.map(x => x.toDouble).sum
+  val budgetTopMax= data.map(x => (x.get("budget"), x.get("original_title")) match {
+    case (b, o) => (b.get.toDouble, o.get)
+  }).sortBy(_._1).reverse.take(5)
+  val budgetTopMin= data.map(x => (x.get("budget"), x.get("original_title")) match {
+    case (b, o) => (b.get.toDouble, o.get)
+  }).filter(b=>b._1 !=  0.0).sortBy(_._1).take(10)
+
+  val sumBug0 = budget.map(x => x.toDouble).sum
+  val sumBug = budget.map(x=>x.toDouble).filter(x=>x != 0.0).sum
+  //Cual es el Presupuesto mas alto?
   println("Presupuesto mas alto:")
   val mayor=budgetName.maxBy(_._1)._2
-
   println(mayor + " // " + budgetName.maxBy(_._1)._1 + "\n")
-  println("Promedio")
-  val promBug=sumBug / budget.size
-  println(promBug + "\n")
+  //Promedio contando 0
+  println("Promedio con 0")
+  val promBug0=sumBug0 / budget.size
+  println(promBug0 + "\n")
+  println("Promedio sin 0")
+  val promBug=sumBug / budget.map(x => x.toDouble).count(x => x != 0.0)
+  println(promBug+"\n")
   println("Presupuesto mas bajo:")
-  //Sin 0
+  //Cual es el presupuesto mas bajo con 0?
+  val minW0=budgetName.min
+  println(minW0._2+ " // " + minW0._1 + "\n")
+  //Cual es el Presupuesto mas bajo sin 0?
+  println("Presupuesto mas bajo sin 0:")
   val minBud=budgetName.filter(x => x ._1 != 0.0).min
   println( minBud._2+ " // " + minBud._1 + "\n")
 
-
+  //Diagrama
   val budgetDiagram=List(
-    ("valor mas alto :"+"\n"+budgetName.maxBy(_._1)._2,budgetName.maxBy(_._1)._1 ),
-    ("valor mas bajo :"+"\n"+budgetName.minBy(_._1)._2,minBud._1),
-    ("Promedio", promBug)
-
+    ("Presupuesto mas alto :"+"\n"+budgetName.maxBy(_._1)._2,budgetName.maxBy(_._1)._1 ),
+    ("Presupuesto mas bajo :"+"\n"+budgetName.minBy(_._1)._2,minBud._1),
+    ("Presupuesto mas bajo contando 0:"+"\n" +minW0._2,minW0._1),
+    ("Promedio", promBug) ,
+    ("Promedio con 0", promBug0)
   )
   BarChart(budgetDiagram.map(_._2))
-    .title("Budget")
+    .title("Presupuesto")
     .xAxis(budgetDiagram.map(_._1))
     .yAxis()
     .frame()
@@ -48,6 +65,30 @@ object EstadisticasBasicas extends App {
     .bottomLegend()
     .render()
     .write(new File("/Users/diegojp/Desktop/diagms/Budget.png")
+    )
+
+  //Diagrama Top
+
+  BarChart(budgetTopMax.map(_._1.toDouble))
+    .title("Las 5 Peliculas Con Mayor Presupuesto")
+    .xAxis(budgetTopMax.map(_._2))
+    .yAxis()
+    .ybounds(100000000)
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/BudgetTopMax.png")
+    )
+
+  BarChart(budgetTopMin.map(_._1.toDouble))
+    .title("Las 10 Peliculas Con Menor Presupuesto")
+    .xAxis(budgetTopMin.map(_._2))
+    .yAxis()
+    .ybounds(0.5)
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/BudgetTopMin.png")
     )
   println("--------------------------------------------------------------------------------------------------")
   println("                                                                 ID")
@@ -70,6 +111,12 @@ object EstadisticasBasicas extends App {
   val popularityName = data.map(x => (x.get("popularity"), x.get("original_title")) match {
     case (p, o) => (p.get.toDouble, o.get)
   })
+  val popularityTopMax= data.map(x => (x.get("popularity"), x.get("original_title")) match {
+    case (p, o) => (p.get.toDouble, o.get)
+  }).sortBy(_._1).reverse.take(5)
+  val popularityTopMin= data.map(x => (x.get("popularity"), x.get("original_title")) match {
+    case (p, o) => (p.get.toDouble, o.get)
+  }).sortBy(_._1).take(5)
   val sumPop = pupularity.map(x => x.toDouble).sum
 
   println("La popularidad mas alta es:")
@@ -77,24 +124,47 @@ object EstadisticasBasicas extends App {
   println("Promedio")
   val promPop=sumPop / pupularity.size
   println( promPop+ "\n")
+  println("La Popularidad mas baja sin 0 es:")
+  val minPopW0=popularityName.minBy(_._1)
+  println(minPopW0._2 + " // " + minPopW0._1+ "\n")
   println("La popularidad mas baja es:")
   val minPop=popularityName.filter(x=>x._1 != 0.0).min
   println( minPop._2 + " // " + minPop._1 + "\n")
 
   val popDiagram=List(
-    ("valor mas alto :"+"\n"+popularityName.maxBy(_._1)._2,popularityName.maxBy(_._1)._1.toDouble ),
-    ("valor mas bajo :"+"\n"+popularityName.minBy(_._1)._2,minPop._1.toDouble),
+    ("Popularidad mas alta :"+"\n"+popularityName.maxBy(_._1)._2,popularityName.maxBy(_._1)._1.toDouble ),
+    ("Popularidad mas baja :"+"\n"+minPop._2,minPop._1.toDouble),
+    ("Popularidad mas baja sin 0: "+"\n"+minPopW0._2,minPopW0._1),
     ("Promedio", promPop)
   )
   BarChart(popDiagram.map(_._2))
-    .title("Popularity")
+    .title("Popularidad")
     .xAxis(popDiagram.map(_._1))
     .yAxis()
+    .ybounds(5)
     .frame()
     .yLabel("Valor")
     .bottomLegend()
     .render()
     .write(new File("/Users/diegojp/Desktop/diagms/Popularity.png")
+    )
+  //Diagrama Top
+  BarChart(popularityTopMax.map(_._1))
+    .title("Top 5 popularidades mas altas")
+    .xAxis(popularityTopMax.map(_._2))
+    .yAxis()
+    .frame()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/PopularityTopMax.png")
+    )
+
+  BarChart(popularityTopMin.map(_._1))
+    .title("Top 5 popularidades mas bajas")
+    .xAxis(popularityTopMin.map(_._2))
+    .yAxis()
+    .frame()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/PopularityTopMin.png")
     )
 
   println("--------------------------------------------------------------------------------------------------")
@@ -103,6 +173,12 @@ object EstadisticasBasicas extends App {
   val revenueName = data.map(x => (x.get("revenue"), x.get("original_title")) match {
     case (r, o) => (r.get.toDouble, o.get)
   })
+  val revenueMax=data.map(x => (x.get("revenue"), x.get("original_title")) match {
+    case (r, o) => (r.get.toDouble, o.get)
+  }).sortBy(_._1).reverse.take(5)
+  val revenueMin=data.map(x => (x.get("revenue"), x.get("original_title")) match {
+    case (r, o) => (r.get.toDouble, o.get)
+  }).filter(r=>r._1 != 0.0).sortBy(_._1).take(5)
   val sumRev = revenue.map(x => x.toDouble).sum
 
   println("La recaudacion mas alta es:")
@@ -115,9 +191,13 @@ object EstadisticasBasicas extends App {
   val minRev=revenueName.filter(x=>x._1 != 0.0).min
   println( minRev._2+ " // " +  minRev._1+ "\n")
 
+  println("La recaudacion mas baja contando 0:")
+  val minRevW0=revenueName.minBy(_._1)
+  println(minRevW0._2+" // "+minRevW0._1)
   val revDiagram=List(
-    ("valor mas alto :"+"\n"+revenueName.maxBy(_._1)._2,revenueName.maxBy(_._1)._1.toDouble ),
-    ("valor mas bajo :"+"\n"+minRev._2,minRev._1.toDouble),
+    ("La recaudacion mas alta es:"+"\n"+revenueName.maxBy(_._1)._2,revenueName.maxBy(_._1)._1.toDouble ),
+    ("La recaudacion mas baja:"+"\n"+minRev._2,minRev._1.toDouble),
+    ("La recaudacion mas baja contando 0:"+"\n"+minRevW0._2,minRevW0._1),
     ("Promedio", promRev)
   )
   BarChart(revDiagram.map(_._2))
@@ -130,14 +210,45 @@ object EstadisticasBasicas extends App {
     .render()
     .write(new File("/Users/diegojp/Desktop/diagms/Revenue.png")
     )
+  //Diagramas Top
+  BarChart(revenueMax.map(_._1))
+    .title("Las 5 Peliculas con mas recaudacion")
+    .xAxis(revenueMax.map(_._2))
+    .yAxis()
+    .ybounds(1000000000)
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/RevenueTopMax.png")
+    )
+
+  BarChart(revenueMin.map(_._1))
+    .title("Las 5 Peliculas con menos recaudacion")
+    .xAxis(revenueMin.map(_._2))
+    .yAxis()
+    .ybounds(1)
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/RevenueTopMin.png")
+    )
 
   println("--------------------------------------------------------------------------------------------------")
   println("                                                                 Calificacion")
 
   val vote = data.flatMap(x => x.get("vote_average"))
   val voteName = data.map(x => (x.get("vote_average"), x.get("original_title")) match {
-    case (v, o) => (v.get, o.get)
+    case (v, o) => (v.get.toDouble, o.get)
   })
+
+  val voteMax= data.map(x => (x.get("vote_average"), x.get("original_title")) match {
+    case (v, o) => (v.get.toDouble, o.get)
+  }).sortBy(_._1).reverse.take(5)
+
+  val voteMin= data.map(x => (x.get("vote_average"), x.get("original_title")) match {
+    case (v, o) => (v.get.toDouble, o.get)
+  }).filter(v=>v._1 != 0.0).sortBy(_._1).take(5)
+
   val sumCal = vote.map(x => x.toDouble).sum
   println("La calificacion mas alta es:")
   println(voteName.maxBy(_._1)._2 + " // " + voteName.maxBy(_._1)._1 + "\n")
@@ -145,12 +256,16 @@ object EstadisticasBasicas extends App {
   val proVote=sumCal / vote.size
   println(proVote + "\n")
   println("La calificacion mas baja es:")
-  val minCal=voteName.filter(x=>x._1 !="0.0").min
+  val minCal=voteName.filter(x=>x._1 != 0.0).min
   println(minCal._2+ " // " + minCal._1+ "\n")
+  println("La calificacion mas baja contando 0 es:")
+  val minCalW0=voteName.minBy(_._1)
+  println(minCalW0._2+" // "+minCalW0._1)
 
   val voteDiagram=List(
-    ("valor mas alto :"+"\n"+voteName.maxBy(_._1)._2,voteName.maxBy(_._1)._1.toDouble ),
-    ("valor mas bajo :"+"\n"+minCal._2,minCal._1.toDouble),
+    ("La calificacion mas alta es:"+"\n"+voteName.maxBy(_._1)._2,voteName.maxBy(_._1)._1.toDouble ),
+    ("La calificacion mas baja es:"+"\n"+minCal._2,minCal._1.toDouble),
+    ("La calificacion mas baja contando 0 es:"+"\n"+minCalW0._2,minCalW0._1),
     ("Promedio", proVote)
   )
   BarChart(voteDiagram.map(_._2))
@@ -163,40 +278,96 @@ object EstadisticasBasicas extends App {
     .render()
     .write(new File("/Users/diegojp/Desktop/diagms/Vote.png")
     )
-
-  /*  println("--------------------------------------------------------------------------------------------------")
-    println("                                                                 Duracion")
-    val runtime = data.flatMap(x => x.get("runtime")).filter(_.contains("."))
-    val runtimeName = data.map(x => (x.get("runtime").filter(_.contains(".")), x.get("original_title")) match {
-      case (r, o) => (r.get, o.get)
-    })
-    println("La duracion mas alta es:")
-    println(runtimeName.maxBy(_._1)._2.map(x=>x.toDouble) + " // " + runtimeName.maxBy(_._1)._1.map(x=>x.toDouble) + "\n")
-    println("Promedio")
-    println("La duracion mas baja es:")
-    val minTime=runtimeName.filter(x=>x._1 != "0.0").min
-    println(minTime._1+ " // " +minTime._2 + "\n")
-
-    val timeDiagram=List(
-      ("valor mas alto :"+"\n"+runtimeName.maxBy(_._1)._2,runtimeName.maxBy(_._1)._1.toDouble ),
-      ("valor mas bajo :"+"\n"+minTime._2,minTime._1.toDouble),
+  //Diagramas Top
+  BarChart(voteMax.map(_._1))
+    .title("Las 5 Peliculas con mejor calificacion")
+    .xAxis(voteMax.map(_._2))
+    .yAxis()
+    .ybounds(9,10)
+    .frame()
+    .yLabel("Valores")
+    .bottomLegend()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/VoteTopMax.png")
     )
-    BarChart(timeDiagram.map(_._2))
-      .title("Runtime")
-      .xAxis(timeDiagram.map(_._1))
-      .yAxis()
-      .frame()
-      .yLabel("Valor")
-      .bottomLegend()
-      .render()
-      .write(new File("/Users/diegojp/Desktop/diagms/Runtime.png")
-      )*/
+
+  BarChart(voteMin.map(_._1))
+    .title("Las 5 Peliculas con peor calificacion")
+    .xAxis(voteMin.map(_._2))
+    .yAxis()
+    .ybounds(0.3)
+    .frame()
+    .yLabel("Valores")
+    .bottomLegend()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/VoteTopMin.png")
+    )
+
+  println("--------------------------------------------------------------------------------------------------")
+  println("                                                                 Duracion")
+  val runtime = data.flatMap(x => x.get("runtime")).filter(_.contains(".")).map(x=>x.toDouble)
+  val runtimeMax= data.map(x=>(x("original_title"),x("runtime"))).filter(_._2.contains(".")).sortBy(_._2.toDouble).reverse.take(5)
+  val runtimeMin= data.map(x => (x("original_title"), x("runtime"))).filter(_._2.contains(".")).filter(_._2.toDouble != 0.0).sortBy(_._2.toDouble).take(5)
+
+  println("La duracion mas alta es:")
+  println(runtime.max + "\n")
+  println("La duracion mas baja es:")
+  val minTime=runtime.filter(x=>x != 0.0).min
+  println(minTime +"\n")
+  println("La duracion mas baja con 0 es:")
+  val minTimeW0=runtime.min
+  println(minTimeW0+"\n")
+
+  val timeDiagram=List(
+    ("La duracion mas alta es:"+"\n",runtime.max ),
+    ("La duracion mas baja es:"+"\n",minTime),
+    ("La duracion mas baja con 0 es:", minTimeW0)
+  )
+  BarChart(timeDiagram.map(_._2))
+    .title("Runtime")
+    .xAxis(timeDiagram.map(_._1))
+    .yAxis()
+    .frame()
+    .yLabel("Valor")
+    .bottomLegend()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/Runtime.png")
+    )
+
+  //Diagramss Top
+  BarChart(runtimeMax.map(_._2.toDouble))
+    .title("Las 5 peliculas con mas duracion")
+    .xAxis(runtimeMax.map(_._1))
+    .yAxis()
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/RuntimeMax.png")
+    )
+
+  BarChart(runtimeMin.map(_._2.toDouble))
+    .title("Las 5 peliculas con menos duracion")
+    .xAxis(runtimeMin.map(_._1))
+    .yAxis()
+    .frame()
+    .yLabel("Valores")
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/RuntimeMin.png")
+    )
+
   println("--------------------------------------------------------------------------------------------------")
   println("                                                                 Conteo de Votos")
   val votecount = data.flatMap(x => x.get("vote_count"))
   val votecountName = data.map(x => (x.get("vote_count"), x.get("original_title")) match {
     case (vc, o) => (vc.get.toDouble, o.get)
   })
+  val votecountMax=data.map(x => (x.get("vote_count"), x.get("original_title")) match {
+    case (vc, o) => (vc.get.toDouble, o.get)
+  }).sortBy(_._1).reverse.take(5)
+  val votecountMin=data.map(x => (x.get("vote_count"), x.get("original_title")) match {
+    case (vc, o) => (vc.get.toDouble, o.get)
+  }).filter(v=>v._1 != 0.0).filter(v=>v._1 != 1.0).sortBy(_._1).take(10)
+
   val sumVC = votecount.map(x => x.toDouble).sum
 
   println("El conteo mas alto es:")
@@ -222,4 +393,24 @@ object EstadisticasBasicas extends App {
     .bottomLegend()
     .render()
     .write(new File("/Users/diegojp/Desktop/diagms/Vote-Count.png"))
+
+  BarChart(votecountMax.map(_._1))
+    .title("Top 5 Peliculas con mas votos")
+    .xAxis(votecountMax.map(_._2))
+    .yAxis()
+    .frame()
+    .yLabel("Valores")
+    .bottomLegend()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/Vote-CountMax.png"))
+
+  BarChart(votecountMin.map(_._1))
+    .title("Top 5 Peliculas con mas votos")
+    .xAxis(votecountMin.map(_._2))
+    .yAxis()
+    .frame()
+    .yLabel("Valores")
+    .bottomLegend()
+    .render()
+    .write(new File("/Users/diegojp/Desktop/diagms/Vote-CountMin.png"))
 }
